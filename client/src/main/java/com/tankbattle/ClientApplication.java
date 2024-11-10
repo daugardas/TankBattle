2b707602b1a094a7481bd21abb6da26a646675b9
Daugardas <luksasdaugardas@gmail.com>
2024-11-11 01:09:08 +0200

    fix: no need to use opengl flag

--- client/src/main/java/com/tankbattle/ClientApplication.java
+++ client/src/main/java/com/tankbattle/ClientApplication.java
@@ -4,7 +4,6 @@ import com.tankbattle.controllers.GameManager;
 
 public class ClientApplication {
     public static void main(String[] args) {
-        System.setProperty("sun.java2d.opengl", "true");
         GameManager.getInstance().initialize();
     }
 }
