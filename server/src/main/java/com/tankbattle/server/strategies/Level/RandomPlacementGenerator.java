774c2dc8665a64108f15ddb7c23f2b9431e3470f
Daugardas <luksasdaugardas@gmail.com>
2024-12-03 22:32:52 +0200

    refactor: convert LevelGenerator interface to abstract class and implement template methods for level generation

--- server/src/main/java/com/tankbattle/server/strategies/Level/RandomPlacementGenerator.java
+++ server/src/main/java/com/tankbattle/server/strategies/Level/RandomPlacementGenerator.java
@@ -6,70 +6,68 @@ import com.tankbattle.server.models.tiles.Tile;
 
 import java.util.Random;
 
-public class RandomPlacementGenerator implements LevelGenerator {
+public class RandomPlacementGenerator extends LevelGenerator {
     private final Random random = new Random();
     private final int wallDensity; // percentage of level occupied by walls
     private final int indestructibleWallChance;
-    private int width;
-    private int height;
 
     public RandomPlacementGenerator() {
+        super(10, 10);
         this.wallDensity = 30;
         this.indestructibleWallChance = 5;
-        this.width = 10;
-        this.height = 10;
     }
 
     public RandomPlacementGenerator(int wallDensity, int indestructibleWallChance) {
+        super(10, 10);
         this.wallDensity = wallDensity;
         this.indestructibleWallChance = indestructibleWallChance;
-        this.width = 10;
-        this.height = 10;
     }
 
     @Override
-    public void setWidth(int width) {
-        this.width = width;
+    protected boolean shouldAddObstacles() {
+        return true;
     }
 
     @Override
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    @Override
-    public Level generateLevel(int width, int height, TileFactory groundFactory, TileFactory destructibleFactory, TileFactory indestructibleFactory, TileFactory liquidFactory) {
-        this.width = width;
-        this.height = height;
-
-        Level level = new Level(width, height);
-        // place wall randomly based on density
+    protected void addObstacles(Level level, TileFactory destructibleFactory, TileFactory indestructibleFactory) {
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                Tile tile;
+                // skip corner tiles for spawnpoints
+                if ((x == 0 && y == 0) || (x == width - 1 && y == 0) || (x == 0 && y == height - 1)
+                        || (x == width - 1 && y == height - 1)) {
+                    continue;
+                }
+
+                Tile tile = level.getTile(x, y);
                 if (random.nextInt(100) < wallDensity) {
-//                        tile = random.nextInt(100) < indestructibleWallChance ? indestructibleFactory.createTile()
-//                                : destructibleFactory.createTile();
+                    tile = random.nextInt(100) < indestructibleWallChance ? indestructibleFactory.createTile()
+                            : destructibleFactory.createTile();
                     tile = destructibleFactory.createTile();
-                } else {
-                    tile = groundFactory.createTile();
                 }
 
                 level.setTile(x, y, tile);
             }
         }
-
-        // set corner tiles to ground tiles
-        level.setTile(0, 0, groundFactory.createTile());
-        level.setTile(width - 1, 0, groundFactory.createTile());
-        level.setTile(0, height - 1, groundFactory.createTile());
-        level.setTile(width - 1, height - 1, groundFactory.createTile());
-
-        return level;
     }
 
     @Override
-    public Level generateLevel(TileFactory groundFactory, TileFactory destructibleFactory, TileFactory indestructibleFactory, TileFactory liquidFactory) {
-        return this.generateLevel(this.width, this.height, groundFactory, destructibleFactory, indestructibleFactory, liquidFactory);
+    protected boolean shouldAddLiquids() {
+        return false;
+    }
+
+    @Override
+    // no liquids in this generator
+    protected void addLiquids(Level level, TileFactory liquidFactory) {
+        return;
+    }
+
+    @Override
+    protected void addPowerUps(Level level) {
+        return;
+    }
+
+    @Override
+    protected void addSpawnPoints(Level level) {
+        return;
     }
 }
